5b81e76710ba8a948d97971321325e13922f4630
Sarang-Nambiar <101126190+Sarang-Nambiar@users.noreply.github.com>
2025-03-18 20:20:53 +0800

    chore: Flushed page data to disk if dirty in eviction

--- src/java/simpledb/storage/BufferPool.java
+++ src/java/simpledb/storage/BufferPool.java
@@ -440,5 +440,11 @@ public class BufferPool {
         Frame frame = this.minHeap.poll();
         PageId pidToRemove = frame.get_Page().getId();
         this.pageToFrame.remove(pidToRemove);
+        
+        try {
+            this.flushPage(pidToRemove); // Write to disk if the page is dirty
+        } catch (IOException e) {
+            throw new DbException("IO error while flushing page");
+        }
     }
 }
